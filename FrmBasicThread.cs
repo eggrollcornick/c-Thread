a9ab0250babb8dc199600486bba1087159c53b6b
Erol Corwyn L. Manalo <116326725+eggrollcornick@users.noreply.github.com>
2022-10-28 13:02:11 +0800

    Add files via upload

---
+++ FrmBasicThread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FrmBasicThreadManalo
+{
+    public partial class FrmBasicThread : Form
+    {
+        private Thread threadA, threadB;
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before Starting THread-");
+            MyThreadClass threadClass = new MyThreadClass();
+            ThreadStart NewThread = new ThreadStart(MyThreadClass.Thread1);
+
+            Thread threadA = new Thread(NewThread);
+            threadA.Name = "Tjread A Process";
+
+            Thread threadB = new Thread(NewThread);
+            threadB.Name = "Tjread B Process";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = "-End of Thread-";
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FrmBasicThreadManalo
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int loopers = 0; loopers<6; loopers++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopers);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
